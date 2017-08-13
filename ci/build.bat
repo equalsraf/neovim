4bdd26c0e12d532a79069ad8e1a57a49c2f5defa
Rui Abreu Ferreira <raf-ep@gmx.com>
2017-08-13 17:09:50 +0100

    appveyor: Clean up build script

--- ci/build.bat
+++ ci/build.bat
@@ -1,6 +1,7 @@
 :: These are native MinGW builds, but they use the toolchain inside
 :: MSYS2, this allows using all the dependencies and tools available
-:: in MSYS2, but we cannot build inside the MSYS2 shell.
+:: in MSYS2. But we cannot build inside the MSYS2 shell, because luarocks
+:: does not work in there.
 echo on
 if "%CONFIGURATION%" == "MINGW_32" (
   set ARCH=i686
@@ -9,26 +10,27 @@ if "%CONFIGURATION%" == "MINGW_32" (
   set ARCH=x86_64
   set BITS=64
 )
-:: We cannot have sh.exe in the PATH (MinGW)
-set PATH=%PATH:C:\Program Files\Git\usr\bin;=%
-set PATH=C:\msys64\mingw%BITS%\bin;C:\Windows\System32;C:\Windows;%PATH%
-:: The default cpack in the PATH is not CMake
-set PATH=C:\Program Files (x86)\CMake\bin\cpack.exe;%PATH%
 
-:: Build third-party dependencies
+:: Sanitize the PATH
+:: - We cannot have sh.exe in the PATH (cmake w/mingw32-make)
+:: - Avoid using the wrong versions of python/perl/cmake/cpack
+set PATH=C:\Program Files\Git\cmd;C:\Python35;C:\Python27;C:\msys64\mingw%BITS%\bin;C:\Windows\System32;C:\Windows;
+
+:: Install dependencies
 C:\msys64\usr\bin\bash -lc "pacman --verbose --noconfirm -Su" || goto :error
 C:\msys64\usr\bin\bash -lc "pacman --verbose --noconfirm --needed -S mingw-w64-%ARCH%-cmake mingw-w64-%ARCH%-perl mingw-w64-%ARCH%-diffutils mingw-w64-%ARCH%-unibilium gperf" || goto :error
 
-:: Setup python (use AppVeyor system python)
-C:\Python27\python.exe -m pip install neovim || goto :error
-C:\Python35\python.exe -m pip install neovim || goto :error
-:: Disambiguate python3
+:: Setup python
+:: - use AppVeyor system python instead of msys, see python-greenlet/greenlet#20
+:: - the python executable is python.exe for both v2 and v3, rename as python3.exe
+:: - install the neovim python module (used in the tests)
 move c:\Python35\python.exe c:\Python35\python3.exe
-set PATH=C:\Python35;C:\Python27;%PATH%
-:: Sanity check
+python  -m pip install neovim || goto :error
 python  -c "import neovim; print(str(neovim))" || goto :error
+python3 -m pip install neovim || goto :error
 python3 -c "import neovim; print(str(neovim))" || goto :error
 
+:: Build third-party dependencies
 mkdir .deps
 cd .deps
 cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=RelWithDebInfo ..\third-party\ || goto :error
